ab293bcb23b64ca61e3f3d22903a438293122f9b
Natan <natanmoreira075@gmail.com>
2026-07-30 09:16:37 -0300

    guards

--- src/controller/gastos.controller.ts
+++ src/controller/gastos.controller.ts
@@ -1,28 +1,33 @@
-import { Body, Controller, Get, Post, Query, Delete } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, Delete, UseGuards } from "@nestjs/common";
 import { CreateGasto } from "../dto/create-gasto.dto";
 import { GastosService } from "../service/gastos.service";
 import { CreateCategoria } from "../dto/create-categoria.dto";
 import { CreateGastoFixo } from "../dto/create-gastofixo.dto";
+import { AuthGuard } from "../security/auth.guard";
 
 @Controller("/gastos")
 export class GastosController {
     constructor(private gastoService: GastosService) {}
 
+    @UseGuards(AuthGuard)
     @Post()
     async registrarGasto(@Body() dto: CreateGasto) {
         return this.gastoService.registrarGasto(dto);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     async listarGastosByUser(@Query("idUser") idUser: number) {
         return this.gastoService.gastosByIdUser(+idUser);
     }
 
+    @UseGuards(AuthGuard)
     @Post("/categoria")
     async adicionarCategoria(@Body() dto: CreateCategoria) {
         return this.gastoService.criarCategorias(dto)
     }
 
+    @UseGuards(AuthGuard)
     @Get("/categoria")
     async listarByCategoria(
         @Query("idCategoria") idCategoria: number,
@@ -31,31 +36,37 @@ export class GastosController {
         return this.gastoService.gastosByCategoria(+idCategoria, +idUser);
     }
 
+    @UseGuards(AuthGuard)
     @Post("/fixo")
     async adicionarGastoFixo(@Body() dto: CreateGastoFixo) {
         return this.gastoService.adicionarGastoFixo(dto);
     }
 
+    @UseGuards(AuthGuard)
     @Get("/listar-categorias")
     async allCategoriasByUser(@Query("idUser") idUser: number) {
         return this.gastoService.allCategoriasByUser(+idUser);
     }
 
+    @UseGuards(AuthGuard)
     @Get("/listar-gastosFixos")
     async listarGastoFixosByUser(@Query("idUser") idUser: number) {
         return await this.gastoService.listarGastosFixos(+idUser);
     }
 
+    @UseGuards(AuthGuard)
     @Get("/listar-gastosMes")
     async listarGastosPorMes(@Query("idUser") idUser: number, @Query("mes") mes: string) {
         return this.gastoService.listarGastosPorMes(+idUser, mes);
     }
 
+    @UseGuards(AuthGuard)
     @Delete("/deletarGasto")
     async deletarGasto(@Query("idUser") idUser: number, @Query("idGastos") idGastos: number) {
         return this.gastoService.excluirGasto(+idUser, +idGastos);
     }
 
+    @UseGuards(AuthGuard)
     @Delete("/deletarCategoria")
     async deletarCategoria(@Query("idUser") idUser: number, @Query("idCategoria") idCategoria: number) {
         return this.gastoService.excluirCategoria(+idCategoria,+idUser);
--- src/controller/relatorio.controller.ts
+++ src/controller/relatorio.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { RelatoriosService } from "../service/relatorio.service";
+import { AuthGuard } from "../security/auth.guard";
 
 @Controller("/relatorio")
 export class RelatorioController {
     constructor(private relatorioService: RelatoriosService) {}
 
+    @UseGuards(AuthGuard)
     @Get("/mensal/categoria")
     relatorioMensalByCategoria(
     @Query("idUser") idUser: number, 
@@ -13,6 +15,7 @@ export class RelatorioController {
         return this.relatorioService.gastoMensalByCategoria(+idUser, +idCategoria);
     }
 
+    @UseGuards(AuthGuard)
     @Get("/mensal")
     relatorioMensal(
     @Query("idUser") idUser: number) {
